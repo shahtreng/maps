add491905f0332e72f492448cb31dc213e50d1cc
root <root@debian.makako.org>
2021-03-06 16:20:48 +0100

    primer commit

---
+++ Components/InteractiveLegend/InteractiveLegend/nls/zh-cn/Legend.js
@@ -0,0 +1,46 @@
+// COPYRIGHT © 2018 Esri
+//
+// All rights reserved under the copyright laws of the United States
+// and applicable international laws, treaties, and conventions.
+//
+// This material is licensed for use under the Esri Master License
+// Agreement (MLA), and is bound by the terms of that agreement.
+// You may redistribute and use this code without modification,
+// provided you adhere to the terms of the MLA and include this
+// copyright notice.
+//
+// See use restrictions at http://www.esri.com/legal/pdfs/mla_e204_e300/english
+//
+// For additional information, contact:
+// Environmental Systems Research Institute, Inc.
+// Attn: Contracts and Legal Services Department
+// 380 New York Street
+// Redlands, California, USA 92373
+// USA
+//
+// email: contracts@esri.com
+//
+// See http://js.arcgis.com/4.9/esri/copyright.txt for details.
+define({
+  "widgetLabel": "Interactive Legend",
+  "points": "点",
+  "lines": "线",
+  "polygons": "面",
+  "creatingLegend": "创建图例",
+  "noLegend": "无图例",
+  "dotValue": "1 点 = {value} {unit}",
+  "currentObservations": "当前观测",
+  "previousObservations": "之前的观测",
+  "high": "高",
+  "low": "低",
+  "esriMetersPerSecond": "米/秒",
+  "esriKilometersPerHour": "千米/小时",
+  "esriKnots": "海里/小时",
+  "esriFeetPerSecond": "英尺/秒",
+  "esriMilesPerHour": "英里/小时",
+  "showNormField": "{field} 除以 {normField}",
+  "showNormPct": "{field} 为总数的百分比",
+  "showRatio": "{field} 与 {normField} 的百分比",
+  "showRatioPercent": "{field} 为 {normField} 的百分比",
+  "showRatioPercentTotal": "{field} 为 {field} 与 {normField} 的百分比"
+});
